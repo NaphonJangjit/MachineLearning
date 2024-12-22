757a86d9c034fff7d95fca423f64b8165e2f6d0f
NaphonJangjit <135398698+NaphonJangjit@users.noreply.github.com>
2024-12-22 18:33:41 +0700

    Update Main.java

--- src/main/java/naphon/ml/Main.java
+++ src/main/java/naphon/ml/Main.java
@@ -10,8 +10,6 @@ import naphon.ml.nn.optimizer.StochasticGradientDescentOptimizer;
 import java.util.Arrays;
 import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         DataFrame df = new DataFrame("x1","x2", "y");
